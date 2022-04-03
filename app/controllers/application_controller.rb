483db291be7176b00ef40a062009525a576db268
Jennifer Pazos <jmp521@gmail.com>
2022-04-03 18:25:32 -0500

    added initial square setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    require 'square'
+
+
+    def initialize_square
+        client = Square::Client.new(
+        access_token: ENV['SQUARE_ACCESS_TOKEN'],
+        environment: 'sandbox'
+        #change to production when ready
+        )
+    end
+
 end
