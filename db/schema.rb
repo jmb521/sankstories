9662bf3e8afc1f8c2ab2b4dba31184f04fec449e
Jennifer Pazos <jmp521@gmail.com>
2022-04-03 17:46:24 -0500

    added migration to be able to store isbn number

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_03_222149) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_03_224536) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_03_222149) do
     t.string "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "isbn"
   end
 
   create_table "orders", force: :cascade do |t|
