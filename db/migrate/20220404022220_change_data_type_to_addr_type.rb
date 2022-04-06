b38df629acd40d278881d1f433234347a2b66cdf
Jennifer Pazos <jmp521@gmail.com>
2022-04-05 20:02:12 -0500

    fixing migrations for production

--- db/migrate/20220404022220_change_data_type_to_addr_type.rb
+++ db/migrate/20220404022220_change_data_type_to_addr_type.rb
@@ -1,6 +1,6 @@
 class ChangeDataTypeToAddrType < ActiveRecord::Migration[7.0]
   def change
-    remove_column :addresses, :type
+    remove_column :addresses, :type, :string
     add_column :addresses, :addr_type, :string
   end
 end
--- db/migrate/20220405031823_remove_purchaser_id_from.rb
+++ db/migrate/20220405031823_remove_purchaser_id_from.rb
@@ -1,5 +1,5 @@
 class RemovePurchaserIdFrom < ActiveRecord::Migration[7.0]
   def change
-    remove_column :addresses, :purchaser_id
+    remove_column :addresses, :purchaser_id, :integer
   end
 end
